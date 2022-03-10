d5afee6275daab0a4b9d13b938c96a5c136d9a59
csi36869 <chaikimlim@gmail.com>
2022-03-10 17:26:03 +0800

    Add NextLevel()

--- Non-Stop/Assets/Script/MainMenu/Quit.cs
+++ Non-Stop/Assets/Script/MainMenu/Quit.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Quit : MonoBehaviour
 {
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     /// <summary>
     /// A function to quit the application.
     /// </summary>
